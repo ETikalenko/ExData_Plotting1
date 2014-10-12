cd836bdfd80691e5f1c5a716af4f5b98307b9de5
ETikalenko <etikalenko@gmail.com>
2014-10-13 00:31:28 +0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,7 +5,11 @@ if (!file.exists("household_power_consumption.txt")) {
                 unzip("power.zip")      
 }
 #read data from file
-data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", dec = ".", col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), colClasses = c("character", "character", rep("numeric",7)))
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                na.strings = "?", dec = ".", col.names = c("Date","Time","Global_active_power",
+                                                                "Global_reactive_power","Voltage","Global_intensity",
+                                                                "Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+                colClasses = c("character", "character", rep("numeric",7)))
 
 #add new column "DT" in data.frame and transform it to date type
 data$DT <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
